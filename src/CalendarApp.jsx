d241b7619f43fac77970de9c4c1321ae01aea494
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-17 20:42:02 -0600

    Feat. Routes implemented.

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,7 +1,11 @@
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from "./router"
 
 
 export const CalendarApp = () => {
     return (
-        <div>CalendarApp</div>
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
     )
 }
---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+
+export const LoginPage = () => {
+    console.log('Login');
+    return (
+        <div>LoginPage</div>
+    )
+}

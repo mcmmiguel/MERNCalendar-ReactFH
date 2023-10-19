4b91580ae6d8d25cbce3a3a526542689697da29e
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-18 19:16:19 -0600

    Feat (WIP): Add Calendar and first steps setting up it

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,55 @@
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { addHours } from 'date-fns'
+
 import { NavBar } from "../"
+import { getMessagesES, localizer } from '../../helpers'
+
+const events = [{
+
+    title: 'Cumple del jefe',
+    notes: 'Comprar el cake',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Miguel'
+    },
+
+}]
+
 
 export const CalendarPage = () => {
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        console.log({ event, start, end, isSelected });
+
+        const style = {
+            backgroundColor: '#347cf7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white',
+        };
+
+        return {
+            style
+        }
+
+    }
     return (
         <>
             <NavBar />
+
+            <Calendar
+                culture='es'
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+                messages={getMessagesES()}
+                eventPropGetter={eventStyleGetter}
+            />
         </>
     )
 }

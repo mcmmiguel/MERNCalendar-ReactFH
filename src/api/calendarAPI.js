80f8d7bc10a5b456dd5115b2503f2823ab0a9a93
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-24 14:12:08 -0600

    Chore: Set up axios and use of env variables

---
+++ src/api/calendarAPI.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { getEnvVariables } from '../helpers/getEnvVariables';
+
+const { VITE_API_URL } = getEnvVariables();
+
+const calendarAPI = axios.create({
+    baseURL: VITE_API_URL,
+});
+
+
+// Configurar interceptores
+
+
+
+export default calendarAPI;

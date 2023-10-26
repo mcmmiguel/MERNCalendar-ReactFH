a3abe3c877dd2fc36a8012f6e206932775cc2d6d
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-25 21:00:12 -0600

    Test. Not test. Just render FabDelete component

---
+++ __tests__/calendar/components/FabDelete.test.jsx
@@ -0,0 +1,19 @@
+import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react'
+import { FabDelete } from '../../../src/calendar/components/FabDelete';
+import { store } from '../../../src/store';
+
+describe('Pruebas en <FabDelete />', () => {
+
+    test('Debe de mostrar el componente correctamente', () => {
+
+        render(
+            <Provider store={store}>
+                <FabDelete />
+            </Provider>
+        );
+        screen.debug();
+
+    });
+
+});

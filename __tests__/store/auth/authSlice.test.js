9fb404023b24fd4df8077177163979ec370b69e3
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-25 19:38:38 -0600

    Test. States and actions in authSlice

--- __tests__/store/auth/authSlice.test.js
+++ __tests__/store/auth/authSlice.test.js
@@ -1,11 +1,59 @@
-import { authSlice } from "../../../src/store/auth/authSlice";
-import { initialState } from "../../fixtures/authStates";
+import { authSlice, clearErrorMessage, onChecking, onLogin, onLogout } from "../../../src/store/auth/authSlice";
+import { authenticatedState, initialState, notAuthenticatedState } from "../../fixtures/authStates";
+import { testUserCredentials } from "../../fixtures/testUser";
 
 describe('Pruebas en authSlice', () => {
 
     test('Debe de regresar el estado inicial', () => {
 
         expect(authSlice.getInitialState()).toEqual(initialState);
+
+    });
+
+    test('Debe de realizar un login', () => {
+
+        let state = authSlice.getInitialState(initialState);
+        state = authSlice.reducer(state, onLogin(testUserCredentials));
+        expect(state).toEqual({
+            status: 'authenticated',
+            user: testUserCredentials,
+            errorMessage: undefined
+        });
+
+    });
+
+    test('Debe de realizar el logout', () => {
+
+        const state = authSlice.reducer(authenticatedState, onLogout());
+        expect(state).toEqual(notAuthenticatedState);
+
+    });
+
+    test('Debe de realizar el logout CON MENSJAE (payload)', () => {
+
+        const errorMessage = 'Credenciales incorrectas';
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+        expect(state).toEqual({ ...notAuthenticatedState, errorMessage });
+
+    });
+
+    test('Debe de limpiar el error message', () => {
+
+        const errorMessage = 'Credenciales incorrectas';
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+        const newstate = authSlice.reducer(state, clearErrorMessage());
+        expect(newstate.errorMessage).toBeUndefined();
+
+    });
+
+    test('Debe de mostrar el estado en onChecking', () => {
+
+        const state = authSlice.reducer(authenticatedState, onChecking());
+        expect(state).toEqual({
+            status: 'checking',
+            user: {},
+            errorMessage: undefined
+        });
 
     });
 
